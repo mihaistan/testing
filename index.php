1eca297f6ba05fbdd9e4e2d31b71abaac52821d5
Mihai Stan <mihai.stan@algo.travel>
2013-05-30 18:25:03 +0300

    Comis index

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: mihai
+ * Date: 5/30/13
+ * Time: 6:23 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+echo"Index-ul proiectului testing Git";
